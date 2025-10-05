cb40d527ee8f1d6cf35d02a12bb852c1022a99af
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2025-10-05 21:10:44 +0800

    api修改

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useAppStore } from "../stores/appStore";
-// import {ElMessageBox} from "element-plus";
+import {ElMessageBox} from "element-plus";
 
 const appStore = useAppStore();
 
@@ -10,34 +10,29 @@ const api = axios.create({
 
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = appStore.token;
     if (token) {
-      config.headers.Authentication = "Bearer " + appStore.token;
+      config.headers.Authentication = "Bearer " + token;
     }
     return config;
   },
   (error) => {
-    // Handle request errors here
     return Promise.reject(error);
   }
 );
 
 api.interceptors.response.use(
   (response) => {
-    // Handle successful responses here
     return response;
   },
   (error) => {
-    // Handle response errors here
     if (error.response && error.response.status === 401) {
-      console.error("Unauthorized access - redirecting to login");
-      // ElMessageBox.alert('登录已过期，请重新登陆', '提示', {
-      //     confirmButtonText: '确定',
-      //     type: 'warning'
-      // }).then(() => {
-      //     localStorage.removeItem('token')
-      //     window.location.href = '/login'
-      // });
+      ElMessageBox.alert('登录已过期，请重新登陆', '提示', {
+          confirmButtonText: '确定',
+          type: 'warning'
+      }).then(() => {
+          appStore.logout();
+      });
     }
     return Promise.reject(error);
   }
