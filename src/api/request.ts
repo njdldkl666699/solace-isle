a40e9c2af6f6eaffe802f8188ddf321deafae988
南极大陆的可乐 <zhangle666699@qq.com>
2025-10-05 20:55:26 +0800

    完成代理配置

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,46 +1,46 @@
-import axios from "axios"
+import axios from "axios";
 import { useAppStore } from "../stores/appStore";
 // import {ElMessageBox} from "element-plus";
 
 const appStore = useAppStore();
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_API_BASE_URL
-})
+  baseURL: import.meta.env.VITE_BASE_URL || "/api",
+});
 
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authentication = 'Bearer ' + appStore.token;
-        }
-        return config
-    },
-    (error) => {
-        // Handle request errors here
-        return Promise.reject(error)
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authentication = "Bearer " + appStore.token;
     }
-)
+    return config;
+  },
+  (error) => {
+    // Handle request errors here
+    return Promise.reject(error);
+  }
+);
 
 api.interceptors.response.use(
-    (response) => {
-        // Handle successful responses here
-        return response
-    },
-    (error) => {
-        // Handle response errors here
-        if (error.response && error.response.status === 401) {
-            console.error("Unauthorized access - redirecting to login")
-            // ElMessageBox.alert('登录已过期，请重新登陆', '提示', {
-            //     confirmButtonText: '确定',
-            //     type: 'warning'
-            // }).then(() => {
-            //     localStorage.removeItem('token')
-            //     window.location.href = '/login'
-            // });
-        }
-        return Promise.reject(error)
+  (response) => {
+    // Handle successful responses here
+    return response;
+  },
+  (error) => {
+    // Handle response errors here
+    if (error.response && error.response.status === 401) {
+      console.error("Unauthorized access - redirecting to login");
+      // ElMessageBox.alert('登录已过期，请重新登陆', '提示', {
+      //     confirmButtonText: '确定',
+      //     type: 'warning'
+      // }).then(() => {
+      //     localStorage.removeItem('token')
+      //     window.location.href = '/login'
+      // });
     }
-)
+    return Promise.reject(error);
+  }
+);
 
-export default api
+export default api;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import {defineConfig, loadEnv} from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd());
 
   return {
     plugins: [vue()],
     server: {
       port: Number(env.VITE_FRONTEND_PORT || 5173),
+      open: true,
+      disableHostCheck: true,
+      hot: true, //自动保存
+      overlay: {
+        warnings: false,
+        errors: true,
+      },
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_APP_URL,
+          ws: false,
+          secure: false,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
-  }
-})
+  };
+});
